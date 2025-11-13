09bda8f42f088f5e04b4ad4cac693a94f0475723
Michael Hahn <mwhahn@gmail.com>
2025-11-12 17:14:28 -0800

    Set compare=False to avoid breaking snapshots

--- pydantic_ai_slim/pydantic_ai/messages.py
+++ pydantic_ai_slim/pydantic_ai/messages.py
@@ -1009,7 +1009,7 @@ class ModelRequest:
     """The instructions for the model."""
 
     model_request_parameters: Annotated[ModelRequestParametersField, pydantic.Field(exclude=True, repr=False)] = field(
-        default=None, repr=False
+        default=None, repr=False, compare=False
     )
     """Full request parameters captured for this request.
 
@@ -1017,7 +1017,7 @@ class ModelRequest:
     """
 
     model_settings: Annotated[ModelSettingsField, pydantic.Field(exclude=True, repr=False)] = field(
-        default=None, repr=False
+        default=None, repr=False, compare=False
     )
     """Effective model settings that were applied to this request.
 
